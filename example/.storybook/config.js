71ee610bf1113cb475261752fd2405f311567529
Praveen Bisht <prvnbist@gmail.com>
2020-01-07 23:02:45 +0530

    feat: added addons

--- example/.storybook/config.js
+++ example/.storybook/config.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { configure, addDecorator, addParameters } from '@storybook/react'
 import { themes } from '@storybook/theming'
+import { withA11y } from '@storybook/addon-a11y'
 
 import { HeapThemeProvider } from '@heapapp/ui'
 import '../src/global.css'
@@ -17,11 +18,15 @@ const ThemeDecorator = storyFn => (
 )
 
 addDecorator(ThemeDecorator)
+addDecorator(withA11y)
 
 addParameters({
    options: {
+      showNav: true,
+      showPanel: true,
+      panelPosition: 'bottom',
       theme: heapTheme
    }
 })
 
-configure(require.context('../src', true, /\.stories\.js$/), module)
+configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module)
